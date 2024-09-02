ac57d74176a29c2a07972075566c72c4d474ed84
Giacomo Fenzi <giacomofenzi@outlook.com>
2024-09-02 07:50:13 +0200

    For 128 benches

--- poly-commit/benches/hyrax_times.rs
+++ poly-commit/benches/hyrax_times.rs
@@ -1,13 +1,13 @@
 use ark_pcs_bench_templates::*;
 use ark_poly::DenseMultilinearExtension;
 
-use ark_bn254::{Fr, G1Affine};
+use ark_bls12_381::{Fr, G1Affine};
 use ark_poly_commit::hyrax::HyraxPC;
 
-// Hyrax PCS over BN254
-type Hyrax254 = HyraxPC<G1Affine, DenseMultilinearExtension<Fr>>;
+// Hyrax PCS over BLS381
+type HyraxBLS381 = HyraxPC<G1Affine, DenseMultilinearExtension<Fr>>;
 
 const MIN_NUM_VARS: usize = 24;
 const MAX_NUM_VARS: usize = 25;
 
-bench!(Hyrax254, rand_ml_poly, rand_ml_point);
+bench!(HyraxBLS381, rand_ml_poly, rand_ml_point);
--- poly-commit/benches/kzg_times.rs
+++ poly-commit/benches/kzg_times.rs
@@ -1,17 +1,17 @@
 use ark_ec::pairing::Pairing;
 use ark_pcs_bench_templates::*;
 
-use ark_bn254::Bn254;
+use ark_bls12_381::Bls12_381;
 use ark_poly::univariate::DensePolynomial as DenseUnivariatePoly;
 use ark_poly_commit::marlin_pc::MarlinKZG10;
 
-type UniPoly = DenseUnivariatePoly<<Bn254 as Pairing>::ScalarField>;
+type UniPoly = DenseUnivariatePoly<<Bls12_381 as Pairing>::ScalarField>;
 
 // KZG over BN254
 #[allow(non_camel_case_types)]
-type KZG_BN254 = MarlinKZG10<Bn254, UniPoly>;
+type KZG_BLS381 = MarlinKZG10<Bls12_381, UniPoly>;
 
 const MIN_NUM_VARS: usize = 24;
 const MAX_NUM_VARS: usize = 25;
 
-bench!(KZG_BN254, rand_uv_poly, rand_uv_point);
+bench!(KZG_BLS381, rand_uv_poly, rand_uv_point);
